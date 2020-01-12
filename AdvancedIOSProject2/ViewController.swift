8cdc9ab95950d47572b7335730db7f80fe64d277
sandeep <sandeep.mhal6@gmail.com>
2020-01-12 00:05:52 -0500

    Initial Commit

---
+++ AdvancedIOSProject2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdvancedIOSProject2
+//
+//  Created by SanDEV on 2020-01-12.
+//  Copyright © 2020 SanDEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
